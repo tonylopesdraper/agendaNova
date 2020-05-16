3e51e4bf1cd1c3a89deae09250ca0eb8fc7af428
tonylopesdraper <61954031+tonylopesdraper@users.noreply.github.com>
2020-05-16 10:22:04 -0300

    Initial commit

---
+++ alma/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from alma.models import Evento
+# Register your models here.
+class EventoAdmin(admin.ModelAdmin):
+    list_display = ('id','titulo', 'data_evento', 'data_criacao')
+    list_filter = ('usuario','data_evento')
+admin.site.register(Evento, EventoAdmin)
